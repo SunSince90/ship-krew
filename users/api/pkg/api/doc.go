a3b958905e1bae37c31739f88ceaea52c6ffbd88
Elis Lulja <elis.lulja@gmail.com>
2021-12-29 16:08:45 +0100

    Define users API types
    Signed-off-by: Elis Lulja <elis.lulja@gmail.com>

---
+++ users/api/pkg/api/doc.go
@@ -0,0 +1,3 @@
+// Package api contains data and types about the users API.
+// TODO: this is a temporary package and will be moved to a better one.
+package api
---
+++ users/api/pkg/api/types.go
@@ -0,0 +1,22 @@
+package api
+
+import (
+	"net"
+	"time"
+)
+
+// TODO: write documentations
+
+type User struct {
+	ID             uint       `gorm:"primaryKey;<-:false" json:"id" yaml:"id"`
+	CreatedAt      time.Time  `json:"created_at" yaml:"created_at"`
+	UpdatedAt      time.Time  `json:"updated_at" yaml:"updated_at"`
+	DeletedAt      *time.Time `gorm:"index" json:"deleted_at,omitempty" yaml:"deleted_at,omitempty"`
+	Username       string     `gorm:"unique;size:100" json:"username" yaml:"username"`
+	DisplayName    string     `gorm:"unique;size:100" json:"display_name" yaml:"display_name"`
+	Email          string     `gorm:"unique;size:100" json:"email" yaml:"email"`
+	RegistrationIP *net.IP    `json:"registration_ip" yaml:"registrationIP"`
+	Joined         time.Time  `json:"joined" yaml:"joined"`
+	Bio            *string    `json:"bio,omitempty" yaml:"bio,omitempty"`
+	Birthday       *time.Time `json:"birthday,omitempty" yaml:"birthday,omitempty"`
+}
