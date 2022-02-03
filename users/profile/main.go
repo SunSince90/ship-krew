0d4aed111eb8fc32c908e66db61c9ad696e58c06
Elis Lulja <elis.lulja@gmail.com>
2022-02-03 21:51:24 +0100

    feat: Get a simple html for users list
    Signed-off-by: Elis Lulja <elis.lulja@gmail.com>

---
+++ users/profile/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"flag"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html"
+	"github.com/rs/zerolog"
+)
+
+const (
+	fiberAppName string = "Profile Backend"
+)
+
+var (
+	log zerolog.Logger
+)
+
+type Elems struct {
+	Color string
+	Val   string
+}
+
+func main() {
+	var (
+		verbosity    int
+		usersApiAddr string
+		timeout      time.Duration
+	)
+
+	flag.IntVar(&verbosity, "verbosity", 1, "the verbosity level")
+
+	// TODO: https, not http
+	flag.StringVar(&usersApiAddr, "users-api-address", "http://users-api", "the address of the users server API")
+	flag.DurationVar(&timeout, "timeout", 2*time.Minute, "requests timeout")
+	flag.Parse()
+
+	log = zerolog.New(os.Stderr).With().Logger()
+	log.Info().Int("verbosity", verbosity).Msg("starting...")
+
+	{
+		logLevels := [4]zerolog.Level{zerolog.DebugLevel, zerolog.InfoLevel, zerolog.ErrorLevel}
+		log = log.Level(logLevels[verbosity])
+	}
+
+	engine := html.New("./views", ".html")
+
+	// TODO: authenticate to users server with APIKey
+
+	app := fiber.New(fiber.Config{
+		AppName:               fiberAppName,
+		ReadTimeout:           time.Minute,
+		DisableStartupMessage: verbosity > 0,
+		Views:                 engine,
+	})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("index", fiber.Map{
+			"Title":  "Hello, World!",
+			"Things": []Elems{{Color: "red", Val: "one"}, {Color: "blue", Val: "two"}},
+		})
+	})
+
+	app.Get("/u/:username", func(c *fiber.Ctx) error {
+		// TODO:
+		return c.SendStatus(fiber.StatusOK)
+	})
+
+	go func() {
+		if err := app.Listen(":8080"); err != nil {
+			log.Err(err).Msg("error while listening")
+		}
+	}()
+
+	// Graceful Shutdown
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	<-stop
+
+	log.Info().Msg("shutting down...")
+	if err := app.Shutdown(); err != nil {
+		log.Err(err).Msg("error while waiting for server to shutdown")
+	}
+	log.Info().Msg("goodbye!")
+}
